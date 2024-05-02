9bbf587dac381f30b1ac8bd6bb2b2481c32dd3b3
hrodriguezg5 <hrodriguezg5@miumg.edu.gt>
2024-05-02 13:43:48 -0600

    Plantilla Finalizada

---
+++ paginaresponsive/app.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#icono-menu");
+let navegacion = document.querySelector(".navegacion");
+
+menu.addEventListener("click", function(){
+    navegacion.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navegacion.classList.remove("active");
+}
